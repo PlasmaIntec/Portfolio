1af069f5276e02a5f90b589789f5580233c742cf
Plasma Intec <plasmaintec@i.dorm.skynet.local>
2019-10-06 21:46:03 -0700

    Add Header

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import Header from './Header.jsx';
+import '../styles/App.css';
 
 export default class App extends Component {
     render() {
         return (
-            <>TODO</>
+            <>
+                <Header />
+            </>
         )
     }
 }
